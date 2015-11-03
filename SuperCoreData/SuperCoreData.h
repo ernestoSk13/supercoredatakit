e999da5f38b5764a237fbd884cadbbdfd89cc326
ernesto sanchez <ernestosanchez1@gdla-mac-lt-151006b.ad.unosquare.com>
2015-11-03 08:49:16 -0600

    Initial Commit

---
+++ SuperCoreData/SuperCoreData.h
@@ -0,0 +1,19 @@
+//
+//  SuperCoreData.h
+//  SuperCoreData
+//
+//  Created by ernesto sanchez on 11/3/15.
+//  Copyright © 2015 ernesto sanchez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SuperCoreData.
+FOUNDATION_EXPORT double SuperCoreDataVersionNumber;
+
+//! Project version string for SuperCoreData.
+FOUNDATION_EXPORT const unsigned char SuperCoreDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SuperCoreData/PublicHeader.h>
+
+
